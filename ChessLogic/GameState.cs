e5018fe8ea5523889a41b606b1e351f264eaef23
AftabAhmedFahim <aftabahmed.official004@gmail.com>
2026-06-30 20:42:14 +0600

    feat: Pieces & The Board
    - Created abstract Piece base class
    - Implemented derived classes for all 6 piece types
    - Created Board class with 8x8 2D array representation
    - Wrote initial piece placement logic

---
+++ ChessLogic/GameState.cs
@@ -0,0 +1,15 @@
+﻿namespace ChessLogic
+{
+    public class GameState
+    {
+        public Board Board { get; }
+        public Player CurrentPlayer { get; private set; }
+
+        // makes easier to test out certain scenarios
+        public GameState(Player player, Board board)
+        {
+            CurrentPlayer = player;
+            Board = board;
+        }
+    }
+}
