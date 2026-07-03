50ddc01b6861c8d6c94674bcac6ae9ef6373407a
AftabAhmedFahim <aftabahmed.official004@gmail.com>
2026-07-04 01:39:09 +0600

    feat: Game Over Menu II
    - Implemented logic to display winner and reason
    - Hooked up menu display to Checkmate/Stalemate triggers
    - Finalized Restart button behavior to reset the board

--- ChessUI/GameOverMenu.xaml.cs
+++ ChessUI/GameOverMenu.xaml.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ChessLogic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ChessUI
 {
@@ -18,19 +9,58 @@ namespace ChessUI
     /// </summary>
     public partial class GameOverMenu : UserControl
     {
-        public GameOverMenu()
+        public event Action<Option> OptionSelected;
+
+        public GameOverMenu(GameState gameState)
         {
             InitializeComponent();
+
+            Result result = gameState.Result;
+            WinnerText.Text = GetWinnerText(result.Winner);
+            ReasonText.Text = GetReasonText(result.Reason, gameState.CurrentPlayer);
+        }
+
+        private static string GetWinnerText(Player winner)
+        {
+            return winner switch
+            {
+                Player.White => "White wins!",
+                Player.Black => "Black wins!",
+                _ => "It's a draw!"
+            };
+        }
+
+        private static string PlayerString(Player player)
+        {
+            return player switch
+            {
+                Player.White => "White",
+                Player.Black => "Black!",
+                _ => ""
+            };
+        }
+
+        private static string GetReasonText(EndReason reason, Player currentPlayer)
+        {
+            return reason switch
+            {
+                EndReason.Stalemate => $"Stalemate! {PlayerString(currentPlayer)} has no legal moves.",
+                EndReason.Checkmate => $"Checkmate! {PlayerString(currentPlayer)} is in check and has no legal moves.",
+                EndReason.FiftyMoveRule => "Draw! Fifty moves have been made without a pawn move or a capture.",
+                EndReason.InsufficientMaterial => "Draw! Neither player has enough material to checkmate the other.",
+                EndReason.ThreefoldRepetition => "Draw! The same position has occurred three times.",
+                _ => ""
+            };
         }
 
         private void Restart_Click(object sender, RoutedEventArgs e)
         {
-
+            OptionSelected?.Invoke(Option.Restart);
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
-
+            OptionSelected?.Invoke(Option.Exit);
         }
     }
 }
---
+++ ChessUI/Option.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessUI
+{
+    public enum Option
+    {
+        Restart,
+        Exit,
+        Continue
+    }
+}
